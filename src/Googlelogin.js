b9aa522aafa5228c3e502eb43c8ca86cfdaf56a2
yunbini <kelly_0925@naver.com>
2024-02-16 15:23:43 +0900

    feat: 토큰값 axios로 전송

--- src/Googlelogin.js
+++ src/Googlelogin.js
@@ -1,5 +1,8 @@
 import {GoogleLogin} from "@react-oauth/google";
 import {GoogleOAuthProvider} from "@react-oauth/google";
+import Cookies from "js-cookie";
+import axios from 'axios';
+// import { response } from "express";
 
 const Googlelogin = () => {
     const clientId = '756646843649-gg9n9b7fvtttk7pg6sd20mcriadmn2bp.apps.googleusercontent.com';
@@ -8,6 +11,18 @@ const Googlelogin = () => {
             <GoogleOAuthProvider clientId={clientId}>
                 <GoogleLogin
                     onSuccess={(res) => {
+                        Cookies.set('accessToken',res.credential);
+
+                    axios.post('35.208.142.216:8080/login',{
+                        token: res.credential
+                        })
+                        .then(response =>{
+                            console.log('응답 데이터: ',response.data);
+                        })
+                        .catch(error =>{
+                            console.log('에러 발생: ',error);
+                        })
+
                         console.log(res);
                     }}
                     onFailure={(err) => {
