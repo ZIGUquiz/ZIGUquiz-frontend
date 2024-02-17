6df22ac2a3c1657598746f1a64003bd95a66fa0a
Sim-GyuBin <skb0606@gmail.com>
2024-02-17 23:16:01 +0900

    Feat: 국가 선택 후 db에 전송 (배포 시 주석 해제 필수)

--- src/component/Country.js
+++ src/component/Country.js
@@ -1,27 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar';
+import Select from 'react-select';
+import Cookies from 'js-cookie';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+const country = require('country-list');
 
 const Country = ({ setPage }) => {
+
+    const navigate = useNavigate();
+
+    const [selectedCountry, setSelectedCountry] = useState(null);
+    const [countryOptions, setCountryOptions] = useState([]);
+        
+    useEffect(() => {
+        const options = country.getNames().map((name) => ({
+            value: country.getCode(name),
+            label: name
+        }));
+        setCountryOptions(options);
+    }, []);
+
+    const handleChange = selectedOption => {
+        setSelectedCountry(selectedOption);
+    };
+
+    const handleSubmit = () => {
+        // // @@@@@@배포 전에 주석 해제 필수 @@@@@@@@@@@@@
+        // if (Cookies.get('accessToken') && selectedCountry) {
+        //     axios.post('http://localhost:8080/user/set-country', {
+        //         nationality: selectedCountry.value.toLowerCase()
+        //     }, { withCredentials: true })
+        //     .then(response => {
+        //         console.log(response.data);
+        //         navigate("/main");
+        //     })
+        //     .catch(error => {
+        //         console.error('There was an error!', error);
+        //     });
+        // }
+    };
+
     return (
         <div className='startPageLayout' style={{ position: 'absolute' }}>
-            <Navbar/>
+            <Navbar />
             <div className='startmainbox'>
                 <div className='maintext'>
                     <div className='choose'>Choose your country</div>
                 </div>
-                <select className='selectbox'>
+                {/* <select className='selectbox'>
                     <option value="korea" className='option'>Korea</option>
                     <option value="korea" className='option'>japen</option>
                     <option value="korea" className='option'>india</option>
-                </select>
-                <div className='submit'>
+                </select> */}
+                <Select
+                    className='selectbox'
+                    value={selectedCountry}
+                    onChange={handleChange}
+                    options={countryOptions}
+                    placeholder="Select a country..."
+                    isSearchable={true}
+                />
+                <div className='submit' onClick={handleSubmit}>
                     Submit
                 </div>
-
             </div>
         </div>
     );
 };
 
 export default Country;
-
