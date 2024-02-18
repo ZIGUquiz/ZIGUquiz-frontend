9fd798afe25ed9d091875ab686b7bbc05bf378e2
yunbini <kelly_0925@naver.com>
2024-02-18 20:49:58 +0900

    feat: 지구 로고 클릭하면 메인 페이지로 이동

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,10 +1,11 @@
 import logo from './img/logo.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return(
         <div>
             <div className='logopic'>
-                <img src={logo}/>
+                <Link to="/"><img src={logo}/></Link>
             </div>
         </div>
     )
